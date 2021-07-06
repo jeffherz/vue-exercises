08174b226e35d6c9bc09e5ed6470e727bb416456
JJHerz <jeffherz565@gmail.com>
2021-07-05 22:20:28 -0400

    Added some more Vue elements to Random User Generator

--- RANDOM-USER-GEN/app.js
+++ RANDOM-USER-GEN/app.js
@@ -1,6 +1,15 @@
 
 const app = Vue.createApp({
-    template: '<h1>Hello World</h1>',
+    
+    data() {
+        return{
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'John@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    }
 })
 
 app.mount('#app')
